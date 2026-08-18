27eb1aecc5715a9636642b93444d507cf3dbe3ec
Abdullah <torgodly@gmail.com>
2026-08-18 13:35:53 +0200

    Add canAccessPanel method to User model
    - Introduced a new method `canAccessPanel` in the User model, which currently returns true, allowing for future access control logic to be implemented.
    
    This update lays the groundwork for managing user access to different panels within the application.

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
