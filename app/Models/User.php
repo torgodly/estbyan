09885ca917f80d61c0aef6f4ea436a2aa2bf74ef
Abdullah <torgodly@gmail.com>
2026-08-18 13:36:35 +0200

    Update User model to include Filament Panel import
    - Added the `Filament\Panel` import to the User model, preparing for future integration with Filament's panel features.
    
    This update enhances the User model's capabilities for panel management.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Filament\Panel;
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
